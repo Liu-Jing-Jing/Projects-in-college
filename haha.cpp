c7e10edac00997e65d53cf7be8b317acda808742
Liu Jing <marklewis@qq.com>
2016-03-22 11:13:03 +0800

    first cpp code

---
+++ haha.cpp
@@ -0,0 +1,20 @@
+#define ίχίχίχ main
+#define ίχίχ int
+#define ίχ (
+#define ίχίχΞΨ )
+#define ίχίχίχίχ {
+#define ίχΞΨίχ }
+#define ίχΞΨίχΞΨίχ printf
+#define ΞΨ "\n"
+#define ίχΞΨίχΞΨ return
+#define ΞΨίχ 0
+#define ίχΞΨ ;
+#define ssss getchar
+#include <stdio.h>
+
+ίχίχ ίχίχίχ ίχ ίχίχΞΨ
+ίχίχίχίχ
+ίχΞΨίχΞΨίχ ίχ "ίχίχίχ! " ΞΨ ίχίχΞΨ ίχΞΨ
+ssss ίχ  ίχίχΞΨ ίχΞΨ
+ίχΞΨίχΞΨ ΞΨίχ ίχΞΨ
+ίχΞΨίχ
